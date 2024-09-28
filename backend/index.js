09023bd56438c67cce5dc0247ffce10f95da2a58
kidusshoa <captainkevin2008@gmail.com>
2024-09-29 02:12:00 +0300

    basic auth routes

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 
 import { connectDB } from "./db/connectDB.js";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, (req, res) => {
   connectDB();
