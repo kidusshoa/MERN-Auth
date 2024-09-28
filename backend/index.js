82907f4683a399cf9c914439d7e118cadf67e30e
kidusshoa <captainkevin2008@gmail.com>
2024-09-29 01:34:05 +0300

    port

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 import express from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server is running on port 3000");
+});
