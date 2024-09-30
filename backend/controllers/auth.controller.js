fa913efc711efd4e50b7421aaef1dfe1115b5957
kidusshoa <captainkevin2008@gmail.com>
2024-09-30 18:08:29 +0300

    jwt token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -29,6 +29,17 @@ export const signup = async (req, res) => {
     });
 
     await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
   } catch (error) {
     return res
       .status(400)
